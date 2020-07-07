c70e7128a5b1014c16fccd0bf53bcd98c899bcfb
MasoudNiki <masoud.niki79@gmail.com>
2020-07-07 17:33:18 +0430

    server aded

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once (__DIR__."/"."../vendor/autoload.php");
+
+    set_time_limit(0);
+    use Ratchet\Http\HttpServer;
+    use Ratchet\Server\IoServer;
+    use Ratchet\WebSocket\WsServer;
+    use App\Chat;
+
+
+
+
+
+
+    $server = IoServer::factory(
+        new HttpServer(new WsServer(new Chat())),
+        8080
+    );
+    $server->run();
